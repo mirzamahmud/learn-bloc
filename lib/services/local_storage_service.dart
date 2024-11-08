826774b9d80e6db6ea571d3f93671496cf89492e
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2024-11-09 00:04:25 +0600

    create_local_storage_service_class

---
+++ lib/services/local_storage_service.dart
@@ -0,0 +1,38 @@
+import 'package:get_storage/get_storage.dart';
+
+class LocalStorageService{
+  /// =============== instance of the singleton
+  static final LocalStorageService _instance = LocalStorageService._internal();
+  /// =============== instance of GetStorage
+  late GetStorage _storage;
+
+  /// =============== private constructor for the singleton
+  LocalStorageService._internal(){
+    _storage = GetStorage();
+  }
+
+  /// =============== factory constructor to return the instance
+  factory LocalStorageService(){
+    return _instance;
+  }
+
+  // ================ method to initialize GetStorage
+  static Future<void> init() async {
+    await GetStorage.init();
+  }
+
+  // ================ set data in storage
+  Future<void> writeData(String key, dynamic value) async {
+    await _storage.write(key, value);
+  }
+
+  // ================ get data from storage
+  dynamic readData(String key) {
+    return _storage.read(key);
+  }
+
+  // ================ delete data from storage
+  Future<void> deleteData(String key) async {
+    await _storage.remove(key);
+  }
+}
